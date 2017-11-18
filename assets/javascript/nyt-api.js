6dbe4bb184caffbc59ce29d9b607694e70665789
Peter Burns <peterburnshicks@gmail.com>
2017-11-18 16:06:26 -0500

    made a bootstrap layout and linked up the javascript

--- assets/javascript/nyt-api.js
+++ assets/javascript/nyt-api.js
@@ -1,4 +1,5 @@
-$('#submit').on('click',function(){
+$('#search').on('click',function(event){
+    event.preventDefault();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     queryURL+='?api-key=ea2923a76ac64cfc9f0dfdf6dda3cc85';
     if($('#searchParameters').val()){
@@ -19,30 +20,32 @@ $('#submit').on('click',function(){
         for(var i = 0;i<howMany;i++){
             var newDiv = $('<div>');
             newDiv.append($('<h3><span class="label label-primary">'+(i+1)+'</span><strong>' + docsArray[i].headline.main + '</strong></h3>'));
-            newDiv.append($('<p>' + docsArray[i].byline.original + '</p>'));
+            //newDiv.append($('<p>' + docsArray[i].byline.original + '</p>'));
             newDiv.append($('<p>' + docsArray[i].pub_date + '</p>'));
             newDiv.append($('<a href=' + docsArray[i].web_url + '>' + docsArray[i].web_url + '</a>'))
             $('#articleDivs').append(newDiv);
         }
     });
 });
-$('#clear').on('click',function(){
+$('#clear').on('click',function(event){
+    event.preventDefault();
     $('.searchElements').val('');
+    $('#articleDivs').empty();
 });
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-queryURL+='?api-key=ea2923a76ac64cfc9f0dfdf6dda3cc85';
-var howMany = 5;
-$.ajax({
-    url:queryURL,
-    method:'GET'
-}).done(function(response){
-    var docsArray = response.response.docs;
-    for(var i = 0;i<howMany;i++){
-        var newDiv = $('<div class="row">');
-        newDiv.append($('<h3><span class="label label-primary">'+(i+1)+'</span><strong>' + docsArray[i].headline.main + '</strong></h3>'));
-        newDiv.append($('<p>' + docsArray[i].byline.original + '</p>'));
-        newDiv.append($('<p>' + docsArray[i].pub_date + '</p>'));
-        newDiv.append($('<a href=' + docsArray[i].web_url + '>' + docsArray[i].web_url + '</a>'))
-        $('#articleDivs').append(newDiv);
-    }
-});
+// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+// queryURL+='?api-key=ea2923a76ac64cfc9f0dfdf6dda3cc85';
+// var howMany = 5;
+// $.ajax({
+//     url:queryURL,
+//     method:'GET'
+// }).done(function(response){
+//     var docsArray = response.response.docs;
+//     for(var i = 0;i<howMany;i++){
+//         var newDiv = $('<div class="row">');
+//         newDiv.append($('<h3><span class="label label-primary">'+(i+1)+'</span><strong>' + docsArray[i].headline.main + '</strong></h3>'));
+//         newDiv.append($('<p>' + docsArray[i].byline.original + '</p>'));
+//         newDiv.append($('<p>' + docsArray[i].pub_date + '</p>'));
+//         newDiv.append($('<a href=' + docsArray[i].web_url + '>' + docsArray[i].web_url + '</a>'))
+//         $('#articleDivs').append(newDiv);
+//     }
+// });
