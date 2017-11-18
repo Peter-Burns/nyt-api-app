e7566c17cb84afe81b963521f59dacd438f63663
Peter Burns <peterburnshicks@gmail.com>
2017-11-18 16:21:51 -0500

    added well class to created divs and fixed number of records to display correctly,removed old JS test code

--- assets/javascript/nyt-api.js
+++ assets/javascript/nyt-api.js
@@ -19,7 +19,7 @@ $('#search').on('click',function(event){
     }).done(function(response){
         var docsArray = response.response.docs;
         for(var i = 0;i<howMany;i++){
-            var newDiv = $('<div>');
+            var newDiv = $('<div class="well">');
             newDiv.append($('<h3><span class="label label-primary">'+(i+1)+'</span><strong>' + docsArray[i].headline.main + '</strong></h3>'));
             if(docsArray[i].byline)newDiv.append($('<p>' + docsArray[i].byline.original + '</p>'));
             newDiv.append($('<p>' + docsArray[i].snippet + '</p>'));
@@ -34,20 +34,3 @@ $('#clear').on('click',function(event){
     $('.searchElements').val('');
     $('#articleDivs').empty();
 });
-// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-// queryURL+='?api-key=ea2923a76ac64cfc9f0dfdf6dda3cc85';
-// var howMany = 5;
-// $.ajax({
-//     url:queryURL,
-//     method:'GET'
-// }).done(function(response){
-//     var docsArray = response.response.docs;
-//     for(var i = 0;i<howMany;i++){
-//         var newDiv = $('<div class="row">');
-//         newDiv.append($('<h3><span class="label label-primary">'+(i+1)+'</span><strong>' + docsArray[i].headline.main + '</strong></h3>'));
-//         newDiv.append($('<p>' + docsArray[i].byline.original + '</p>'));
-//         newDiv.append($('<p>' + docsArray[i].pub_date + '</p>'));
-//         newDiv.append($('<a href=' + docsArray[i].web_url + '>' + docsArray[i].web_url + '</a>'))
-//         $('#articleDivs').append(newDiv);
-//     }
-// });
